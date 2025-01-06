5bf6ab480ded9dad6b475170e0fe77dbd06f0706
MohamedKhX <muhamedkhx2@gmail.com>
2025-01-06 12:04:41 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         Table::configureUsing(function (Table $table) {
             $table->defaultSort('created_at', 'desc')->striped();
